832cd722cf90a0c4354b582648795a99c908a878
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-23 18:15:09 -0500

    Creación main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,33 @@
 import 'package:flutter/material.dart';
-import 'package:teoria_flutter/src/holamundo.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primarySwatch: Colors.green),
+      home: RootPage(),
+    );
+  }
+}
+
+class RootPage extends StatefulWidget {
+  const RootPage({super.key});
+
+  @override
+  State<RootPage> createState() => _RootPageState();
+}
+
+class _RootPageState extends State<RootPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
 }
