99fdcf845bf34a0222fd121084965981438b292b
LAPTOP-S37N1HI5\yeison <yeisoncalderonpoveda08@gmail.com>
2024-10-24 16:17:04 -0500

    Mostrar imagenes svg

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,18 +21,16 @@ class MyApp extends StatelessWidget {
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
               SizedBox(
-                  height: 250.0,
-                  width: 350.0,
-                  child: Image.asset(
-                    "assets/oveja.png",
-                    fit: BoxFit.cover,
-                  )),
+                height: 250.0,
+                width: 350.0,
+                child: SvgPicture.asset("assets/2.svg"),
+              ),
               SizedBox(
                   height: 250.0,
                   width: 350.0,
                   child: Image.network(
                     "https://www.minecraft.net/content/dam/minecraftnet/community/events/cy2024/minecraft-movie/Global-Nav_Panel-Secondary_SS_321x321.jpg",
-                    fit: BoxFit.cover,
+                    fit: BoxFit.fitWidth,
                   ))
             ],
           ),
